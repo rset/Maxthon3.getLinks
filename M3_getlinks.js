bdc14866a0526ea41123bcf7eda95058118fc1e6
rset <seydo_@hotmail.com>
2012-11-23 23:57:16 +0200

    Update M3_getlinks.js

--- M3_getlinks.js
+++ M3_getlinks.js
@@ -1,15 +1,12 @@
-/*
-	23/11/12 fri
-*/
 try
 {
 	var Links = document.links;
 	var Pre = document.getElementsByTagName('pre');
 	var i = 0, linkSayisi = Links.length;
 	var downloadLinks = new Array();
-	var stil = "position:fixed;top:0px;font:1.0em Verdana,Helvetica,sans-serif;padding:10px;height:auto;width:auto;z-index:99;background:#FFF8C6;";
+	var stil = "position:fixed;top:0px;font:1.0em Verdana,Helvetica,sans-serif;padding:10px;height:auto;width:auto;z-index:99;background:#CC3333;";
 	var images = document.getElementsByTagName('img');
-
+//FFF8C6
 	for(var ii=0; ii<images.length; ++ii)
 	{
 		images[ii].style.display='none';
@@ -17,13 +14,13 @@ try
 
 	for (i=0; i<linkSayisi; i++)
 	{
-		if(Links[i].href.match(/http(s)?\:\/\/(www\.)?rapid|hotfile|uploaded|ul.to|freakshare|bitshare.+/g) 
+		if(Links[i].href.match(/http(s)?\:\/\/(www\.)?rapid|hotfile|uploaded|ul.to|freakshare|bitshare|rockdizfile.+/g) 
 		&& (!Links[i].href.match(/.+ref|register.+/g)))
 		{
 			downloadLinks.push(Links[i]);		
 		}
 	}
-	
+	//http://rockdizfile.com/78ik31f6nog9/Y%C4%B1ld%C4%B1z_Tilbe_-_Sana_Kalbim_Ge%C3%A7ti_(2012)mp3yolu.org.mp3.html
 	var downloadLinksSayi = downloadLinks.length;
 	
 	if ( downloadLinksSayi>0)
